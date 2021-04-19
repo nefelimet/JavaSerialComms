bf6a19794264cc1b311fd4b5977f730291d2f032
Nefeli Metallidou <47782076+nefelimet@users.noreply.github.com>
2021-04-19 13:26:49 +0300

    Added image and GPS requests. Added print boolean argument to methods listen and sendAndListen.
    1. Changed request codes since the first session ended.
    2. In the listen and sendAndListen methods, added a boolean argument that tells us if we want to print the rxmessage or not.
    3. Added image request and GPS request code. The image isn't printed on the monitor since the file is too big. We save it in a txt file for the moment. In the future we will see how to convert that into a jpg file. The GPS codes aren't yet printed correctly on the monitor or the txt file, so that needs fixing.

--- userApp.java
+++ userApp.java
@@ -4,12 +4,12 @@ import java.io.IOException;  // Import the IOException class to handle errors
 
 public class userApp {
 
-	public static String echoRequestCode =  "E1092";
-	public static String imageRequestCode = "M2490";
-	public static String imageRequestCodeError = "G0342";
-	public static String gpsRequestCode = "P4642";
-	public static String ackCode = "Q2289";
-	public static String nackCode = "R8766";
+	public static String echoRequestCode =  "E0223";
+	public static String imageRequestCode = "M8880";
+	public static String imageRequestCodeError = "G6145";
+	public static String gpsRequestCode = " P7268";
+	public static String ackCode = " Q9717";
+	public static String nackCode = "R6418";
 
 	public static void main(String[] param) {
 		(new userApp()).demo();
@@ -24,7 +24,7 @@ public class userApp {
 		return modem;
 	}
 
-	public String listen(Modem modem, String delimiter){
+	public String listen(Modem modem, String delimiter, boolean printRx){
 		int k;
 		String rxmessage ="";
 		System.out.println("Sent request for message.");
@@ -43,7 +43,7 @@ public class userApp {
 				//System.out.println(rxmessage);
 				//Break endless loop by catching break sequence.
 				if (rxmessage.indexOf(delimiter)>-1){																		//If the break sequence is found break
-					System.out.println(rxmessage);
+					if(printRx==true){System.out.println(rxmessage);}
 					System.out.println("End of listen message.");
 					break;
 				}
@@ -54,7 +54,7 @@ public class userApp {
 		return rxmessage;
 	}
 
-	public String sendAndListen(Modem modem, String txmessage, String delimiter){
+	public String sendAndListen(Modem modem, String txmessage, String delimiter, boolean printRx){
 		int k;
 		String rxmessage = "";
 		//We create a different variable for the message we will send to Ithaki. The reason for differentiating is because for some reason the terminal couldn't print the
@@ -63,7 +63,7 @@ public class userApp {
 		modem.write(txmessageToSend.getBytes());
 		String toPrint = "Sent " + txmessage + " request code.";
 		System.out.println(toPrint);
-		rxmessage = listen(modem, delimiter);
+		rxmessage = listen(modem, delimiter, printRx);
 		return rxmessage;
 	}
 
@@ -98,20 +98,33 @@ public class userApp {
 		Modem modem = initModem(1000, 2000);
 
 		//Listen for welcome message
-		String welcomeMessage = listen(modem, "\r\n\n\n");
+		String welcomeMessage = listen(modem, "\r\n\n\n", true);
 
 		//Send a test message and listen for answer
-		String testMessage = sendAndListen(modem, "test", "PSTOP\r\n");
+		String testMessage = sendAndListen(modem, "test", "PSTOP\r\n", true);
 
 		//Create text file to store echo messages
 		createFile("echopackets.txt");
+		//createFile("imagepacket.txt");
+		createFile("gpspackets.txt");
 		writeToFile("echopackets.txt", "Echo packets received: \n\n");
+		//writeToFile("imagepacket.txt", "Image packet received: \n\n");
+		writeToFile("gpspackets.txt", "GPS packets received: \n\n");
 
 		//Send echoRequestCodes and listen for answers. Write them to echopackets.txt file.
-		for(int i =0; i<5; i++){
-			String rxmessage = sendAndListen(modem, echoRequestCode, "PSTOP");
+		for(int i =0; i<2; i++){
+			String rxmessage = sendAndListen(modem, echoRequestCode, "PSTOP", true);
 			writeToFile("echopackets.txt", rxmessage+"\r\n");
 		}
+
+		//Send imageRequestCode and listen for answer. Write the rxmessage into txt file.
+		// String rxmessage = sendAndListen(modem, imageRequestCode, "PSTOP", false);
+		// writeToFile("imagepacket.txt", rxmessage+"\r\n");
+
+		//Send gpsRequestCode and listen for answer. Write the rxmessage to gpspacket.txt file.
+		String gpsMessage = sendAndListen(modem, gpsRequestCode, "STOP ITHAKI GPS TRACKING\r", true);
+		writeToFile("gpspackets.txt", gpsMessage+"\r\n");
+
 
 		//Close modem
 		modem.close();
